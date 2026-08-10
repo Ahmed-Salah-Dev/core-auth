451be57d32fec7b50e77cffca3aa2e20de664f40
ibnsalah <ahmed.a.salah24@gmail.com>
2026-08-10 22:41:25 +0300

    test: integrate Laravel Testbench

--- tests/Unit/CoreAuthServiceProviderTest.php
+++ tests/Unit/CoreAuthServiceProviderTest.php
@@ -5,20 +5,21 @@ declare(strict_types=1);
 namespace Tests\Unit;
 
 use AhmedSalahDev\CoreAuth\CoreAuthServiceProvider;
-use Illuminate\Container\Container;
-use PHPUnit\Framework\TestCase;
+use Orchestra\Testbench\TestCase;
 
 final class CoreAuthServiceProviderTest extends TestCase
 {
-    public function test_service_provider_can_be_instantiated(): void
+    protected function getPackageProviders($app): array
     {
-        $container = new Container();
-
-        $provider = new CoreAuthServiceProvider($container);
-
-        $this->assertInstanceOf(
+        return [
             CoreAuthServiceProvider::class,
-            $provider
+        ];
+    }
+
+    public function test_service_provider_is_registered(): void
+    {
+        $this->assertTrue(
+            $this->app->getProvider(CoreAuthServiceProvider::class) !== null
         );
     }
 }
